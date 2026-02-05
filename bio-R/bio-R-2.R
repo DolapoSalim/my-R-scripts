e4d840d270f93c2a8dcc5cc810412a64f1c70423
Dolapo Olatoye <dolaposalim@gmail.com>
2026-02-05 11:37:26 +0100

    install packages for bioR classes

---
+++ bio-R/bio-R-2.R
@@ -0,0 +1,35 @@
+# BioR — 2nd Class setup (run once)
+
+
+
+pkgs <- c(
+  "tidyverse",   # dplyr, tidyr, ggplot2, readr, purrr, tibble, stringr, forcats
+  "janitor",     # clean_names(), tabyl()
+  "lubridate",   # dates
+  "skimr",       # quick data QA
+  "here",         # project-safe file paths
+  "readxl",      # Excel import
+  "conflicted"   # clearer function conflicts
+)
+
+
+pkgs <- c(pkgs)
+
+
+# Install missing packages
+to_install <- pkgs[!pkgs %in% rownames(installed.packages())]
+if (length(to_install) > 0) {
+  install.packages(to_install, dependencies = TRUE)
+}
+
+
+
+# Load packages 
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(skimr)
+library(here)
+library(readxl)
+library(conflicted)
