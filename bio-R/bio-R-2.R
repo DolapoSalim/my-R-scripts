8155134135064597545f8790e1529cef0928d57c
Dolapo Olatoye <dolaposalim@gmail.com>
2026-02-05 12:15:36 +0100

    updated the BioR script

--- bio-R/bio-R-2.R
+++ bio-R/bio-R-2.R
@@ -1,7 +1,5 @@
 # BioR — 2nd Class setup (run once)
 
-
-
 pkgs <- c(
   "tidyverse",   # dplyr, tidyr, ggplot2, readr, purrr, tibble, stringr, forcats
   "janitor",     # clean_names(), tabyl()
@@ -15,17 +13,16 @@ pkgs <- c(
 
 pkgs <- c(pkgs)
 
-
 # Install missing packages
 to_install <- pkgs[!pkgs %in% rownames(installed.packages())]
+
+# conditional statement for installing package found to be false in to_install
 if (length(to_install) > 0) {
   install.packages(to_install, dependencies = TRUE)
 }
 
 
-
 # Load packages 
-
 library(tidyverse)
 library(janitor)
 library(lubridate)
